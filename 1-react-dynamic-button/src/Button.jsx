ac7c8236e6b80b39a28584a2e2c7b62eb374f9a9
LaupWing <loc_strict@hotmail.com>
2023-12-13 05:26:15 +0100

    boom tailwind

--- 1-react-dynamic-button/src/Button.jsx
+++ 1-react-dynamic-button/src/Button.jsx
@@ -1,7 +1,11 @@
 const Button = ({
    variant = "default"
 }) => {
-
+   const variants = {
+      default: "bg-gray-100 text-gray-800",
+      primary: "bg-blue-500 text-white",
+      danger: "bg-red-500 text-white"
+   }
    return <div>Button</div>
 }
 export default Button
