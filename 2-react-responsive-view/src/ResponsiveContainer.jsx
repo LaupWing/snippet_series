393589901422be80650f30fdc650b113ca256772
LaupWing <loc_strict@hotmail.com>
2023-12-16 07:55:46 +0100

    udpate

--- 2-react-responsive-view/src/ResponsiveContainer.jsx
+++ 2-react-responsive-view/src/ResponsiveContainer.jsx
@@ -1,6 +1,6 @@
 const ResponsiveContainer = ({ children }) => {
    return (
-      <div>{children}</div>
+      <div className="w-full max-w-sm m-4">{children}</div>
    )
 }
 export default ResponsiveContainer
